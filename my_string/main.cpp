16674047dd99bd471b9c0e2f0c105f3123ef4eeb
Bohdan Kladochnyi <bodya1379@gmail.com>
2022-05-06 22:10:14 +0300

    add ctor's, size, cap, c_str, op[], copy, length add some dummy tests

--- my_string/main.cpp
+++ my_string/main.cpp
@@ -5,4 +5,19 @@
 int main()
 {
 
+	containers::String str1;
+	containers::String str2("Hello, world!");
+	for (size_t i = 0; i != str2.size(); ++i)
+		std::cout << str2[i];
+	std::cout << std::endl;
+
+	containers::String str3 = str2;
+	for (size_t i = 0; i != str3.size(); ++i)
+		std::cout << str3[i];
+	std::cout << std::endl;
+
+	str1 = str3;
+	for (size_t i = 0; i != str1.size(); ++i)
+		std::cout << str1[i];
+	std::cout << std::endl;
 }
--- my_string/my_string.h
+++ my_string/my_string.h
@@ -3,7 +3,59 @@
 namespace containers {
 
 class String final {
+	size_t size_ = 0, capacity_ = 0;
+	char* data_ = nullptr;
 
+public:
+	String() = default;
+	String(const char* str)
+		: size_(length(str) + 1), capacity_(size_), data_(new char[capacity_]) {
+		copy(str, str + size_, data_);
+	}
+	String(const String& other)
+		: size_(other.size_), capacity_(other.capacity_), data_(new char[capacity_]) {
+		copy(other.data_, other.data_ + capacity_, data_);
+	}
+	String& operator=(const String& other) {
+		delete[] data_;
+		size_ = other.size_;
+		capacity_ = other.capacity_;
+		data_ = new char[capacity_];
+		copy(other.data_, other.data_ + capacity_, data_);
+		return *this;
+	}
+
+public: //acessors
+	using size_type = size_t;
+
+	size_t size() const {
+		return size_ - 1;
+	}
+	size_t capacity() const {
+		return capacity_;
+	}
+	char* c_str() const {
+		return data_;
+	}
+	char& operator[](size_t pos) {
+		return data_[pos];
+	}
+
+private:
+	char* copy(const char* src_beg, const char* src_end, char* dst_beg) {
+		while (src_beg != src_end) {
+			*dst_beg = *src_beg;
+			++src_beg; ++dst_beg;
+		}
+		return dst_beg;
+	}
+	size_t length(const char* str) {
+		const char* p = str;
+		while (*p) {
+			++p;
+		}
+		return p - str;
+	}
 };
 
 }
